4920622198c6e557fd3746ecabbeba77e2ae3165
Veerabatula Sai Keerti <116068848+vsaikeerti@users.noreply.github.com>
2022-12-06 12:43:02 +0530

    new line to dev

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! this is from COMAKEIT";
+    private final String message = " helle worked Hello World! this is from COMAKEIT";
 
     public App() {}
 
